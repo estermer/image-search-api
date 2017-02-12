ec4597e2376366108b026c03d3716e590c67eda4
estermer <eric.stermer@gmail.com>
2017-02-11 21:54:36 -0500

    set up mongoose connection and global mongoose promises

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express     = require('express');
 const app         = express();
 const mongoose    = require('mongoose');
+const Latest      = require('./latest.model.js');
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/image-search-api';
+mongoose.connect(mongoURI);
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res) => {
   res.json({"status": 200, "message": "Welcome to the image search API."})
